1ebedbe243b7dfa8d06397756e2ae2f941428eca
sake1 <timothydickys@gmail.com>
2019-06-12 07:32:16 +0000

    exec script edit

--- script/execute.sh
+++ script/execute.sh
@@ -1,10 +1,10 @@
-hadoop fs -rm -r -f /user/cloudera/recommender/output* && 
+hadoop fs -rm -r -f /user/recommender/output* && 
 javac -cp /usr/lib/hadoop/*:/usr/lib/hadoop-mapreduce/* -d ../bin/recommender/ ../src/recommender/*.java -Xlint &&
 echo "Java class compilation success." &&
 jar -cvf recommender.jar -C ../bin/recommender/ . &&
 echo "Begin Hadoop MapReduce Job..." &&
-time hadoop jar recommender.jar recommender.Tester \
-/user/cloudera/recommender/input/job_100/ \
-/user/cloudera/recommender/input/user_100/ \
-/user/cloudera/recommender/output \
-/user/cloudera/recommender/output2;
+time yarn jar recommender.jar recommender.Tester -libjars ./json-simple-1.1.1.jar \
+/user/recommender/input/job_30000/ \
+/user/recommender/input/user_100000/ \
+/user/recommender/output \
+/user/recommender/output2;
